efb02bcc952d83983a5155f30520fecdb543d002
NonZErO <theallenanker@gmail.com>
2020-02-07 20:59:00 +0800

    Sovled problem: "Sum of Two Integers".

---
+++ src/com/delicate/leetcode/easy/SumOfTwoIntegers.java
@@ -0,0 +1,11 @@
+package com.delicate.leetcode.easy;
+
+public class SumOfTwoIntegers {
+    public static void main(String[] args) {
+        System.out.println(getSum(1, 2));
+    }
+
+    public static int getSum(int a, int b) {
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
+    }
+}
