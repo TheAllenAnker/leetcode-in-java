b83aa8b6445b21f9be7fa39b983d283fc21f5af0
NonZErO <theallenanker@gmail.com>
2020-02-10 16:27:55 +0800

    Solved problem: "Missing Number".

---
+++ src/com/delicate/leetcode/easy/MissingNumber.java
@@ -0,0 +1,20 @@
+package com.delicate.leetcode.easy;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{3, 0, 1}));
+        System.out.println(missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+    }
+
+    public static int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
