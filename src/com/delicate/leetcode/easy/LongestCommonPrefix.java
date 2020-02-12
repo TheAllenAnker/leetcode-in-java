ee59ff52421f11d68d9fa94b2617de2cc608381e
NonZErO <theallenanker@gmail.com>
2020-02-12 23:04:09 +0800

    Solved problem: "Longest Common Prefix".

---
+++ src/com/delicate/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+package com.delicate.leetcode.easy;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != c) {
+                    return result.toString();
+                }
+            }
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
