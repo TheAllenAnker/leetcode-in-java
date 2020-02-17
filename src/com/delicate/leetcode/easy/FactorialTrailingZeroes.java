31e72ccb32d1d2251891b1f55f8d3129abf17521
NonZErO <theallenanker@gmail.com>
2020-02-17 20:03:28 +0800

    Solved problem: "Factorial Trailing Zeroes".(edited).

--- src/com/delicate/leetcode/easy/FactorialTrailingZeroes.java
+++ src/com/delicate/leetcode/easy/FactorialTrailingZeroes.java
@@ -2,12 +2,12 @@ package com.delicate.leetcode.easy;
 
 public class FactorialTrailingZeroes {
     public static void main(String[] args) {
-
+        System.out.println();
     }
 
     public static int trailingZeroes(int n) {
         int count = 0;
-        while (n > 5) {
+        while (n >= 5) {
             count += (n / 5);
             n /= 5;
         }
