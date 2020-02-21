b5eee9823db47171c6fea71e4ce7695ea909db88
NonZErO <theallenanker@gmail.com>
2020-02-21 16:24:17 +0800

    Solved problem: "Max Depth of Binary Tree".

---
+++ src/com/delicate/leetcode/foroffers/MaxDepthOfBT.java
@@ -0,0 +1,17 @@
+package com.delicate.leetcode.foroffers;
+
+import com.delicate.leetcode.bean.TreeNode;
+
+public class MaxDepthOfBT {
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
