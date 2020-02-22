e509b3cafb0b93418d1d487c00b809bfa852631e
NonZErO <theallenanker@gmail.com>
2020-02-22 11:49:35 +0800

    Solved problem: "Last Remaining".

---
+++ src/com/delicate/leetcode/foroffers/LastRemaining.java
@@ -0,0 +1,22 @@
+package com.delicate.leetcode.foroffers;
+
+public class LastRemaining {
+    public static void main(String[] args) {
+        System.out.println(lastRemaining(5, 3));
+    }
+
+    public static int lastRemaining(int n, int m) {
+        if (n <= 0 || m <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 1) {
+            return 0;
+        }
+
+        int index = 0;
+        for (int i = 2; i <= n; i++) {
+            index = (index + m) % i;
+        }
+        return index;
+    }
+}
