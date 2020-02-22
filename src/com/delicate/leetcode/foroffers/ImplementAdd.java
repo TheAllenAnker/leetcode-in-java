24ab90f82fd7cbe4773727f297bc4e7ce7eea3d8
NonZErO <theallenanker@gmail.com>
2020-02-22 12:01:04 +0800

    Solved problem: "Implement Add".

---
+++ src/com/delicate/leetcode/foroffers/ImplementAdd.java
@@ -0,0 +1,17 @@
+package com.delicate.leetcode.foroffers;
+
+public class ImplementAdd {
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+    }
+
+    public static int add(int a, int b) {
+        while (b != 0) {
+            int xor = (a ^ b);
+            b = (a & b) << 1;
+            a = xor;
+        }
+
+        return a;
+    }
+}
