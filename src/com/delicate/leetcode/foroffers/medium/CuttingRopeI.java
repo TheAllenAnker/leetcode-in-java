6c2fd8590a572f0555742f8ef62fd213567d7fae
NonZErO <theallenanker@gmail.com>
2020-02-23 10:13:08 +0800

    Solved problem: "Cutting Rope I".

---
+++ src/com/delicate/leetcode/foroffers/medium/CuttingRopeI.java
@@ -0,0 +1,19 @@
+package com.delicate.leetcode.foroffers.medium;
+
+public class CuttingRopeI {
+    public static void main(String[] args) {
+
+    }
+
+    public int cuttingRope(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
