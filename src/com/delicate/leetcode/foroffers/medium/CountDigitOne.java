062957992a805007a86bc8ff7b20002c421664a2
NonZErO <theallenanker@gmail.com>
2020-02-24 13:27:56 +0800

    Solved problem: "Count Digit One".

---
+++ src/com/delicate/leetcode/foroffers/medium/CountDigitOne.java
@@ -0,0 +1,16 @@
+package com.delicate.leetcode.foroffers.medium;
+
+public class CountDigitOne {
+    public static void main(String[] args) {
+
+    }
+
+    public int countDigitOne(int n) {
+        int count = 0;
+        for (long i = 1; i <= n; i *= 10) {
+            long divider = i * 10;
+            count += (n / divider) * i + Math.min(Math.max(n % divider - i + 1, 0), i);
+        }
+        return count;
+    }
+}
