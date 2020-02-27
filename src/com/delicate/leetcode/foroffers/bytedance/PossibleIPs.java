15a8d89e0021af1ff6b9dbda9279a9a42e3755bb
NonZErO <theallenanker@gmail.com>
2020-02-27 12:33:39 +0800

    "Solved" problem: "Possible IPs".

---
+++ src/com/delicate/leetcode/foroffers/bytedance/PossibleIPs.java
@@ -0,0 +1,23 @@
+package com.delicate.leetcode.foroffers.bytedance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PossibleIPs {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> possibleIPs(String s) {
+        if (s == null || s.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        int len = s.length();
+        int finalLen = len + 3;
+        int i = 0;
+        List<String> result = new ArrayList<>();
+        // just go through all possible scenarios
+        return result;
+    }
+}
