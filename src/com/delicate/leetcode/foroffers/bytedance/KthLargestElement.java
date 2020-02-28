331cb722b2361adc928048669d43695432a2a142
NonZErO <theallenanker@gmail.com>
2020-02-28 11:11:13 +0800

    Solved problem: "Kth Largest Element".

---
+++ src/com/delicate/leetcode/foroffers/bytedance/KthLargestElement.java
@@ -0,0 +1,20 @@
+package com.delicate.leetcode.foroffers.bytedance;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.add(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.element();
+    }
+}
