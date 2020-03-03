6a5b40c24f7c2039a19addfc95d1715d378ed699
NonZErO <theallenanker@gmail.com>
2020-03-03 10:52:35 +0800

    Solved problem: "Loweset Common Ancestor".

---
+++ src/com/delicate/leetcode/foroffers/bytedance/LowestCommonAncestor.java
@@ -0,0 +1,26 @@
+package com.delicate.leetcode.foroffers.bytedance;
+
+import com.delicate.leetcode.bean.TreeNode;
+
+public class LowestCommonAncestor {
+    private static TreeNode resultNode;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null) {
+            return right;
+        }
+        return left;
+    }
+}
