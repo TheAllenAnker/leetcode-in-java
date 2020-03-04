3028903b11df22ea327f406eca57f8311e129c42
NonZErO <theallenanker@gmail.com>
2020-03-04 16:23:41 +0800

    Solved problem: "Minimum Total".

---
+++ src/com/delicate/leetcode/foroffers/bytedance/MinimumTotal.java
@@ -0,0 +1,24 @@
+package com.delicate.leetcode.foroffers.bytedance;
+
+import java.util.List;
+
+public class MinimumTotal {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+
+        int size = triangle.size();
+        int[] dp = new int[size];
+        for (int i = 0; i < size; i++) {
+            dp[i] = triangle.get(size - 1).get(i);
+        }
+
+        for (int i = size - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
